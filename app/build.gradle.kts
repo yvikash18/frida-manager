8e14c25f1afbd7e98576e59e6c0e6834e7b70159
yvikash18 <yvikash880@gmail.com>
2026-01-04 21:29:22 +0530

    Bump version to 1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.fridamanager.android"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
